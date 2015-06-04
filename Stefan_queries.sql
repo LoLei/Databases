f2e9889aeb254249e4231ddeea98ff098140d3d6
Stefan Braeuer <stefan.braeuer@student.tugraz.at>
2015-06-04 17:48:34 +0200

    more queries

---
+++ Stefan_queries.sql
@@ -0,0 +1,25 @@
+INSERT INTO `mydb`.`locations` (`country`, `city`, `street`, `number`) VALUES ('USA', 'Bellevue', 'Northeast Street', '4');
+
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('1', 'Newell', 'Gabe', '23', 'CEO', 'USA', 'Bellevue', 'Northeast Street', '4', '1');
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('2', 'Walker', 'Robin', '23', 'TF2 Inventor', 'USA', 'Bellevue', 'Northeast Street', '4', '1');
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('3', 'Gates', 'Bill', '65', 'VP', 'USA', 'Bellevue', 'Northeast Street', '4', '1');
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('4', 'Ismail', 'Abdul', '31', 'Dota Inventor', 'USA', 'Bellevue', 'Northeast Street', '4', '1');
+
+INSERT INTO `mydb`.`locations` (`country`, `city`, `street`, `number`) VALUES ('Austria', 'Graz', 'Inffeldgasse', '16');
+INSERT INTO `mydb`.`locations` (`country`, `city`, `street`, `number`) VALUES ('Germany', 'Berlin', 'Hauptstrasse', '1');
+INSERT INTO `mydb`.`locations` (`country`, `city`, `street`, `number`) VALUES ('USA', 'Springfield', 'Fakestreet', '123');
+INSERT INTO `mydb`.`locations` (`country`, `city`, `street`, `number`) VALUES ('USA', 'Silicon Valley', 'Mountain View', '3');
+INSERT INTO `mydb`.`locations` (`country`, `city`, `street`, `number`) VALUES ('Westeros', 'Winterfell', 'Main Road ', '1');
+
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('5', 'Musterman', 'Max', '37', 'Developer', 'USA', 'Bellevue', 'Northeast Street', '4', '2');
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('6', 'Rogrammer', 'Peter', '27', 'Graphic Designer', 'USA', 'Bellevue', 'Northeast Street', '4', '2');
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('7', 'Dude', 'Guy', '52', 'Developer', 'USA', 'Bellevue', 'Northeast Street', '4', '2');
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('8', 'Balmer', 'Steve', '48', 'Developer', 'USA', 'Bellevue', 'Northeast Street', '4', '2');
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('9', 'Jobs', 'Steve', '42', 'Engineer', 'USA', 'Bellevue', 'Northeast Street', '4', '2');
+
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('10', 'Burns', 'Montgomery', '122', 'Manager', 'USA', 'Springfield', 'Fakestreet', '123', '1');
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('11', 'Simpson', 'Homer', '38', 'Safety Inspector', 'USA', 'Springfield', 'Fakestreet', '123', '10');
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('12', 'Simpson', 'Marge', '36', 'House Wife', 'USA', 'Springfield', 'Fakestreet', '123', '10');
+
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('13', 'Jörg', 'Hans', '45', 'Manager', 'Germany', 'Berlin', 'Hauptstrasse', '1', '1');
+INSERT INTO `mydb`.`employees` (`id`, `last_name`, `first_name`, `age`, `position`, `locations_country`, `locations_city`, `locations_street`, `locations_number`, `employees_id`) VALUES ('14', 'Musterfrau', 'Anita', '32', 'Developer', 'Germany', 'Berlin', 'Hauptstrasse', '1', '13');
