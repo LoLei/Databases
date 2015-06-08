e8b755aec500a47310a054e9d5f5cdc844aca30f
LorenzLeitner <lorenz.leitner@student.tugraz.at>
2015-06-08 17:06:17 +0200

    Add more servlets

--- Eclipse/src/org/db/servlets/Start.java
+++ Eclipse/src/org/db/servlets/Start.java
@@ -30,8 +30,88 @@ public class Start extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter writer = response.getWriter();
-		writer.println("Okay guys");
+		writer.println("<html>");
+		writer.println("<head><title>Overview Servlet</title></head>");
+		writer.println("<body>");
 		
+		// Location
+		writer.println("<form method='get' action='/DB2/InsertLocation'>");
+		writer.println("<h1>Insert a new Location:</h1>");
+		writer.println("Country: <input type='text' name='locationcountry'/>");
+		writer.println("City: <input type='text' name='locationcity'/>");
+		writer.println("Street: <input type='text' name='locationstreet'/>");
+		writer.println("Street Number: <input type='text' name='locationnumber'/>");
+		writer.println("<input type='submit' value='Insert location'/>");
+		writer.println("</form>");
+		
+		// Employee
+//		writer.println("<form method='get' action='/DB2/InsertEmployee'>");
+//		writer.println("<h1>Insert a new Employee:</h1>");
+//		writer.println("ID: <input type='text' name='employeeid'/>");
+//		writer.println("Last name: <input type='text' name='employeelast_name'/>");
+//		writer.println("First name: <input type='text' name='employeefirst_name'/>");
+//		writer.println("Age: <input type='text' name='employeeage'/>");
+//		writer.println("Position: <input type='text' name='employeeposition'/>");
+//		writer.println("Country: <input type='text' name='employeecountry'/>");
+//		writer.println("City: <input type='text' name='employeecity'/>");
+//		writer.println("Street: <input type='text' name='employeestreet'/>");
+//		writer.println("Street Number: <input type='text' name='employeenumber'/>");
+//		writer.println("Employee ID (Boss): <input type='text' name='employeeemployeeid'/>");
+//		writer.println("<input type='submit' value='Insert employee'/>");
+//		writer.println("</form>");
+		
+		// Client
+		writer.println("<form method='get' action='/DB2/InsertClient'>");
+		writer.println("<h1>Insert a new Client:</h1>");
+		writer.println("ID: <input type='text' name='clientid'/>");
+		writer.println("Last name: <input type='text' name='clientlast_name'/>");
+		writer.println("First name: <input type='text' name='clientfirst_name'/>");
+		writer.println("Age: <input type='text' name='clientage'/>");
+		writer.println("Employee ID: <input type='text' name='clientemployeeid'/>");
+		writer.println("<input type='submit' value='Insert client'/>");
+		writer.println("</form>");
+		
+		// Products
+		writer.println("<form method='get' action='/DB2/InsertProduct'>");
+		writer.println("<h1>Insert a new Product:</h1>");
+		writer.println("ID: <input type='text' name='productid'/>");
+		writer.println("Name: <input type='text' name='productname'/>");
+		writer.println("Type <input type='text' name='producttype'/>");
+		writer.println("Value: <input type='text' name='productvalue'/>");
+		writer.println("Selling price: <input type='text' name='productselling_price'/>");
+		writer.println("<input type='submit' value='Insert product'/>");
+		writer.println("</form>");
+		
+		// Delete Employee
+		writer.println("<form method='get' action='/DB2/DeleteEmployee'>");
+		writer.println("<h1>Delete a specific Employee:</h1>");
+		writer.println("Employee ID: <input type='text' name='EId'/>");
+		writer.println("<input type='submit' value='Delete Employee'/>");
+		writer.println("</form>");
+		
+		// Get names of employees who are older than x years who use
+		// software with name y and whose 
+		// Position is z
+		writer.println("<form method='get' action='/DB2/GetEmployeeName'>");
+		writer.println("<h1>Get all employee, who used a specific software, are older than a specific age and have a specific position:</h1>");
+		writer.println("Age: <input type='text' name='eage'/>");
+		writer.println("Software name: <input type='text' name='softwarename'/>");
+		writer.println("Position name: <input type='text' name='positionname'/>");
+		writer.println("<input type='submit' value='Submit'/>");
+		writer.println("</form>");
+		
+		// Get names of employees who used vehicle of type x and brand y
+		writer.println("<form method='get' action='/DB2/GetEmployeeName2'>");
+		writer.println("<h1>Get names of employees who used vehicle of type x and brand y</h1>");
+		writer.println("Type: <input type='text' name='vtype'/>");
+		writer.println("Brand name: <input type='text' name='vbrand'/>");
+		writer.println("<input type='submit' value='Submit'/>");
+		writer.println("</form>");
+				
+		
+		writer.println("<body>");
+		writer.println("</html>");
+		writer.close(); 
 	}
 
 	/**
